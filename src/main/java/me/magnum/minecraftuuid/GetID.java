11e36a7120182b45c0f0856d216676c55b1aa506
Magnum <magnum1997@gmail.com>
2019-07-29 21:30:17 -0700

    Click to copy UUID
    Added tool-tip & click to copy function.

--- src/main/java/me/magnum/minecraftuuid/GetID.java
+++ src/main/java/me/magnum/minecraftuuid/GetID.java
@@ -1,12 +1,18 @@
 package me.magnum.minecraftuuid;
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.*;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandCompletion;
 import me.magnum.lib.CheckSender;
 import me.magnum.lib.Common;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import static me.magnum.minecraftuuid.GetUUID.pre;
 
@@ -16,8 +22,7 @@ public class GetID extends BaseCommand {
 	@CommandAlias("getuuid|uuid|getid")
 	@SuppressWarnings({"deprecation"})
 	@CommandCompletion("@Players")
-	@CommandPermission("uuid.get")
-	public void onGet(CommandSender sender, String user) {
+	public void onGet (CommandSender sender, String user) {
 		if (CheckSender.isCommand(sender)) {
 			return;
 		}
@@ -28,12 +33,28 @@ public class GetID extends BaseCommand {
 		OfflinePlayer p = Bukkit.getOfflinePlayer(user);
 		if (p.hasPlayedBefore()) {
 			String uuid = p.getUniqueId().toString();
-			Common.tell(sender, pre + "&FUUID for &e" + user + " &fis &e" + uuid);
-		} else Common.tell(sender, pre + "&f" + user + " &2has not played on this server.");
+			if (!CheckSender.isPlayer(sender)) {
+				Common.tell(sender, pre + "&FUUID for &e" + p.getName() + " &fis &e" + uuid);
+				return;
+			}
+			Player player = (Player) sender;
+			TextComponent msg = new TextComponent();
+			TextComponent hover = new TextComponent();
+			hover.setText("Click to copy UUID");
+			ClickEvent ce = new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, uuid);
+			HoverEvent he = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hover).create());
+			msg.setText(pre.replace('&', '§') + "§fUUID for §e" + p.getName() + " §fis §e" + uuid);
+			msg.setHoverEvent(he);
+			msg.setClickEvent(ce);
+			player.spigot().sendMessage(msg);
+		}
+		else {
+			Common.tell(sender, pre + "&f" + user + " &2has not played on this server.");
+		}
 		
 	}
 	
-	private void onHelp(CommandSender sender) {
+	private void onHelp (CommandSender sender) {
 		Common.tell(sender,
 		            pre + "&F--------------------------------------------",
 		            pre + "&AHow to use GetUUID:",
--- src/main/java/me/magnum/minecraftuuid/GetUUID.java
+++ src/main/java/me/magnum/minecraftuuid/GetUUID.java
@@ -4,20 +4,19 @@ import co.aikar.commands.BukkitCommandManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GetUUID extends JavaPlugin {
-
-    public static String pre = "&7[&aGetUUID&7] ";
-    private static BukkitCommandManager commandManger;
-
-
-    @Override
-    public void onEnable() {
-        registerCommands();
-    }
-
-    private void registerCommands() {
-        commandManger = new BukkitCommandManager(this);
-        commandManger.registerCommand(new GetID());
-
-    }
-
+	
+	static String pre = "&7[&aGetUUID&7] ";
+	public static GetUUID plugin;
+	
+	
+	@Override
+	public void onEnable () {
+		registerCommands();
+	}
+	
+	private void registerCommands () {
+		plugin = this;
+		BukkitCommandManager commandManger = new BukkitCommandManager(plugin);
+		commandManger.registerCommand(new GetID());
+	}
 }
